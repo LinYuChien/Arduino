105340cc6ade69e4e4a22a63a3f98ab9fc80966f
linyuchien <npr286rx@rocketmail.com>
2016-03-04 21:42:57 +0800

    output test

---
+++ files/JFF/output_test/output_test.ino
@@ -0,0 +1,22 @@
+//6位元數字跑馬燈
+
+int BASE = 2;
+int NUM = 6;
+int index = 0;
+
+void setup() {
+  for ( int i = BASE ; i < BASE + NUM ; i++ ){
+    pin Mode( i, OUTPUT );
+  }
+
+}
+
+void loop() {
+  for ( int i = BASE ; i < BASE + NUM ; i++){
+    digitalWrite( i , LOW );
+  }
+
+  digitalWrite( BASE + index , HIGH );
+  index = ( index + 1 ) % NUM ;
+  delay(100);
+}
