c4d5949eb556c64d0bc0c1b0e9d154e2e63b2c42
linyuchien <npr286rx@rocketmail.com>
2016-06-26 20:32:43 +0800

    進みました！　print錯誤IP已解決  新增遠端控制亮度程式

--- files/Ethernet_shield/diy16_01_BUG_print__IP__solved_/diy16_01_BUG_print__IP__solved_.ino
+++ files/Ethernet_shield/diy16_01_BUG_print__IP__solved_/diy16_01_BUG_print__IP__solved_.ino
@@ -1,6 +1,7 @@
 // 使用Webduino程式庫建立微網站
 // 詳細的程式說明，請參閱第十六章，16-2頁。
 //bug:Serail print的IP錯誤  但是可以連線
+//錯誤已解決
 #include "SPI.h"
 #include "Ethernet.h"
 #include "WebServer.h"
@@ -32,8 +33,8 @@ void defaultCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
 
 void setup() {  
   Serial.begin(9600);
-  IPAddress ip = Ethernet.localIP();
-  Ethernet.begin(mac);
+  Ethernet.begin(mac);               //初始化動態IP乙太網路連線
+  IPAddress ip = Ethernet.localIP(); //取得分配到的IP位址  跟上一行次序不可顛倒，否則無法print出正確IP
   webserver.setDefaultCommand(&defaultCmd);   // 處理「首頁」請求
   webserver.begin();
   Serial.println(F( "\nStarting Ethernet..."));
