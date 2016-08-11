63f77bcd0ea1583fe534da028cc85a7a289cd634
linyuchien <npr286rx@rocketmail.com>
2016-08-11 17:13:46 +0800

    edited file

--- files/Esp8266/STA_mode_GPIO_Web_control_LED/Web_control_LED/Web_control_LED.ino
+++ files/Esp8266/STA_mode_GPIO_Web_control_LED/Web_control_LED/Web_control_LED.ino
@@ -1,8 +1,3 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-*********/
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -11,8 +6,8 @@
 MDNSResponder mdns;
 
 // Replace with your network credentials
-const char* ssid = "it.nchu";
-const char* password = "it.nchu@317";
+const char* ssid = "SSID";
+const char* password = "password";
 
 ESP8266WebServer server(80);
 
@@ -22,8 +17,18 @@ int gpio0_pin = 0;
 int gpio2_pin = 2;
 
 void setup(void){
-  webPage += "<h1>ESP8266 Web Server</h1><p>Socket #1 <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></p>";
-  webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
+  webPage += "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>STA模式網頁控制</title>";
+  webPage += "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://goo.gl/6gNKaN\">";
+  webPage += "</head><body><br><div class=\"ui buttons\">";
+  webPage += "<a href=\"L1_OFF\"><button class=\"ui button\">關</button></a>";
+  webPage += "<div class=\"or\" data-text=\"or\"></div>";
+  webPage += "<a href=\"L1_ON\"><button class=\"ui positive button\">開</button></a>";
+  webPage += "</div>";
+  webPage += "<br><br><div class=\"ui buttons\"><a href=\"L2_OFF\"><button class=\"ui button\">關</button></a>";
+  webPage += "<div class=\"or\" data-text=\"or\"></div>";
+  webPage += "<a href=\"L2_ON\"><button class=\"ui positive button\">開</button></a>";
+  webPage += "</div></body></html>";
+  
   
   // preparing GPIOs
   pinMode(gpio0_pin, OUTPUT);
@@ -54,22 +59,22 @@ void setup(void){
   server.on("/", [](){
     server.send(200, "text/html", webPage);
   });
-  server.on("/socket1On", [](){
+  server.on("/L1_ON", [](){
     server.send(200, "text/html", webPage);
     digitalWrite(gpio0_pin, HIGH);
     delay(1000);
   });
-  server.on("/socket1Off", [](){
+  server.on("/L1_OFF", [](){
     server.send(200, "text/html", webPage);
     digitalWrite(gpio0_pin, LOW);
     delay(1000); 
   });
-  server.on("/socket2On", [](){
+  server.on("/L2_ON", [](){
     server.send(200, "text/html", webPage);
     digitalWrite(gpio2_pin, HIGH);
     delay(1000);
   });
-  server.on("/socket2Off", [](){
+  server.on("/L2_OFF", [](){
     server.send(200, "text/html", webPage);
     digitalWrite(gpio2_pin, LOW);
     delay(1000); 
