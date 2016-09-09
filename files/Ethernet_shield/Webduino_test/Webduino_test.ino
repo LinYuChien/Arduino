05c1c53321660902657d55795b640d0428cb7a7c
linyuchien <npr286rx@rocketmail.com>
2016-09-09 11:09:39 +0800

    Webduino test

--- files/Ethernet_shield/Webduino_test/Webduino_test.ino
+++ files/Ethernet_shield/Webduino_test/Webduino_test.ino
@@ -1,22 +1,19 @@
 // 使用Webduino程式庫建立微網站
-// 詳細的程式說明，請參閱第十六章，16-2頁。
-//bug:Serail print的IP錯誤  但是可以連線
-//錯誤已解決
 #include "SPI.h"
 #include "Ethernet.h"
 #include "WebServer.h"
 
 static byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xFD };
-//IPAddress ip(192, 168, 0,102);
-//IPAddress subnet(255, 255, 255, 0);
-//IPAddress gateway(192, 168, 1, 1);
+IPAddress ip(192, 168, 0,102);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress gateway(192, 168, 1, 1);
 
 WebServer webserver("", 80);
 
 P(homePage) = 
     "<!doctype html>"
     "<html><head><meta charset=\"UTF-8\" />"
-    "<title>Arduino XDD</title>"
+    "<title>Webduino test</title>"
     "</head><body>"
     "This is index test"
     "</body></html>";
